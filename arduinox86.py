f056e481c05ebd14e4e3373284f9e5d0f18610c9
Nicolás Pernas Maradei <nicolas.pernas.maradei@emutex.com>
2014-07-16 17:26:23 +0100

    Inital docstring
    Add initial documentation in the form of docstrings to public API.
    Apply autopep8 to the code.

--- arduinox86.py
+++ arduinox86.py
@@ -1,50 +1,49 @@
 # -*- coding: utf-8 -*-
+"""
+ Copyright © 2014, Emutex Ltd.
+ All rights reserved.
+ http://www.emutex.com
+
+ Author: Nicolás Pernas Maradei <nicolas.pernas.maradei@emutex.com>
+ Author: Dave Hunt <dave@emutex.com>
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions
+ are met:
+
+ 1. Redistributions of source code must retain the above copyright
+    notice, this list of conditions and the following disclaimer.
+
+ 2. Redistributions in binary form must reproduce the above copyright
+    notice, this list of conditions and the following disclaimer in
+    the documentation and/or other materials provided with the
+    distribution.
+
+ 3. All modifications to the source code must be clearly marked as
+    such. Binary redistributions based on modified source code must
+    be clearly marked as modified versions in the documentation and/or
+    other materials provided with the distribution.
+
+ 4. The name of Emutex Ltd. may not be used to endorse or promote
+    products derived from this software without specific prior written
+    permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES INCLUDING,
+ BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ POSSIBILITY OF SUCH DAMAGE.
+"""
+
 import datetime
 import os
-
-#########################################################################
-# Copyright © 2014, Emutex Ltd.
-# All rights reserved.
-# http://www.emutex.com
-#
-# Author: Nicolás Pernas Maradei <nicolas.pernas.maradei@emutex.com>
-# Author: Dave Hunt <dave@emutex.com>
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-#
-# 1. Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-#
-# 2. Redistributions in binary form must reproduce the above copyright
-#    notice, this list of conditions and the following disclaimer in
-#    the documentation and/or other materials provided with the
-#    distribution.
-#
-# 3. All modifications to the source code must be clearly marked as
-#    such. Binary redistributions based on modified source code must
-#    be clearly marked as modified versions in the documentation and/or
-#    other materials provided with the distribution.
-#
-# 4. The name of Emutex Ltd. may not be used to endorse or promote
-#    products derived from this software without specific prior written
-#    permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES INCLUDING,
-# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-#
-#########################################################################
 
 INPUT = 'in'
 INPUT_PULLUP = 'in_pullup'
@@ -58,7 +57,12 @@ DRIVE_HIZ = 'hiz'
 
 
 class GPIOGalileoGen2(object):
-    
+
+    """Class for managing GPIO pinout on Intel® Galileo Gen2 board
+
+    TODO: longer description here
+    """
+
     GPIO_MAPPING = {
         0: 11,
         1: 12,
@@ -70,128 +74,163 @@ class GPIOGalileoGen2(object):
         7: 2,
         8: 3,
         9: 4,
-       10: 10,
-       11: 5,
-       12: 15,
-       13: 7,
-       14: 48,
-       15: 50,
-       16: 52,
-       17: 54,
-       18: 56,
-       19: 58,
+        10: 10,
+        11: 5,
+        12: 15,
+        13: 7,
+        14: 48,
+        15: 50,
+        16: 52,
+        17: 54,
+        18: 56,
+        19: 58,
     }
 
     GPIO_MUX_OUTPUT = {
-        0: ( (32, LOW), (33, NONE) ),
-        1: ( (45, LOW), (28, LOW), (29, NONE) ),
-        2: ( (77, LOW), (34, LOW), (35, NONE) ),
-        3: ( (64, LOW), (76, LOW), (16, LOW), (17, NONE) ),
-        4: ( (36, LOW), (37, NONE) ),
-        5: ( (66, LOW), (18, LOW), (19, NONE) ),
-        6: ( (68, LOW), (20, LOW), (21, NONE) ),
-        7: ( (38, LOW), (39, NONE) ), #FIXME update for Fab-G
-        8: ( (40, LOW), (41, NONE) ), #FIXME update for Fab-G
-        9: ( (70, LOW), (22, LOW), (23, NONE) ), 
-       10: ( (74, LOW), (26, LOW), (27, NONE) ),
-       11: ( (44, LOW), (72, LOW), (24, LOW), (25, NONE) ),
-       12: ( (42, LOW), (43, NONE) ),
-       13: ( (46, LOW), (30, LOW), (31, NONE) ),
-       14: ( (49, NONE), ),
-       15: ( (51, NONE), ),
-       16: ( (53, NONE), ),
-       17: ( (55, NONE), ),
-       18: ( (78, HIGH), (60, HIGH), (57, NONE) ), #FIXME not working
-       19: ( (79, HIGH), (60, HIGH), (59, NONE) ), #FIXME not working
+        0: ((32, LOW), (33, NONE)),
+        1: ((45, LOW), (28, LOW), (29, NONE)),
+        2: ((77, LOW), (34, LOW), (35, NONE)),
+        3: ((64, LOW), (76, LOW), (16, LOW), (17, NONE)),
+        4: ((36, LOW), (37, NONE)),
+        5: ((66, LOW), (18, LOW), (19, NONE)),
+        6: ((68, LOW), (20, LOW), (21, NONE)),
+        7: ((38, LOW), (39, NONE)),  # FIXME update for Fab-G
+        8: ((40, LOW), (41, NONE)),  # FIXME update for Fab-G
+        9: ((70, LOW), (22, LOW), (23, NONE)),
+        10: ((74, LOW), (26, LOW), (27, NONE)),
+        11: ((44, LOW), (72, LOW), (24, LOW), (25, NONE)),
+        12: ((42, LOW), (43, NONE)),
+        13: ((46, LOW), (30, LOW), (31, NONE)),
+        14: ((49, NONE), ),
+        15: ((51, NONE), ),
+        16: ((53, NONE), ),
+        17: ((55, NONE), ),
+        18: ((78, HIGH), (60, HIGH), (57, NONE)),  # FIXME not working
+        19: ((79, HIGH), (60, HIGH), (59, NONE)),  # FIXME not working
     }
 
     GPIO_MUX_INPUT = {
-        0: ( (32, HIGH), (33, NONE) ),
-        1: ( (45, LOW), (28, HIGH), (29, NONE) ),
-        2: ( (77, LOW), (34, HIGH), (35, NONE) ), # FIXME: input always connected
-        3: ( (64, LOW), (76, LOW), (16, HIGH), (17, NONE) ),  # FIXME: input always connected
-        4: ( (36, HIGH), (37, NONE) ),
-        5: ( (66, LOW), (18, HIGH), (19, NONE) ),
-        6: ( (68, LOW), (20, HIGH), (21, NONE) ),
-        7: ( (38, HIGH), (39, NONE) ), #TODO update for Fab-G # FIXME: input always connected
-        8: ( (40, HIGH), (41, NONE) ), #TODO update for Fab-G # FIXME: input always connected
-        9: ( (70, LOW), (22, HIGH), (23, NONE) ),
-       10: ( (74, LOW), (26, HIGH), (27, NONE) ),
-       11: ( (44, LOW), (72, LOW), (24, HIGH), (25, NONE) ),
-       12: ( (42, HIGH), (43, NONE) ),
-       13: ( (46, LOW), (30, HIGH), (31, NONE) ),
-       14: ( (49, NONE), ), #FIXME
-       15: ( (51, NONE), ), #FIXME
-       16: ( (53, NONE), ), #FIXME
-       17: ( (55, NONE), ), #FIXME
-       18: ( (78, HIGH), (60, HIGH), (57, NONE) ), # FIXME: input always connected
-       19: ( (79, HIGH), (60, HIGH), (59, NONE) ), # FIXME: input always connected
+        0: ((32, HIGH), (33, NONE)),
+        1: ((45, LOW), (28, HIGH), (29, NONE)),
+        2: ((77, LOW), (34, HIGH), (35, NONE)),  # FIXME: input always connected
+        3: ((64, LOW), (76, LOW), (16, HIGH), (17, NONE)),  # FIXME: input always connected
+        4: ((36, HIGH), (37, NONE)),
+        5: ((66, LOW), (18, HIGH), (19, NONE)),
+        6: ((68, LOW), (20, HIGH), (21, NONE)),
+        7: ((38, HIGH), (39, NONE)),  # TODO update for Fab-G # FIXME: input always connected
+        8: ((40, HIGH), (41, NONE)),  # TODO update for Fab-G # FIXME: input always connected
+        9: ((70, LOW), (22, HIGH), (23, NONE)),
+        10: ((74, LOW), (26, HIGH), (27, NONE)),
+        11: ((44, LOW), (72, LOW), (24, HIGH), (25, NONE)),
+        12: ((42, HIGH), (43, NONE)),
+        13: ((46, LOW), (30, HIGH), (31, NONE)),
+        14: ((49, NONE), ),  # FIXME
+        15: ((51, NONE), ),  # FIXME
+        16: ((53, NONE), ),  # FIXME
+        17: ((55, NONE), ),  # FIXME
+        18: ((78, HIGH), (60, HIGH), (57, NONE)),  # FIXME: input always connected
+        19: ((79, HIGH), (60, HIGH), (59, NONE)),  # FIXME: input always connected
     }
 
     GPIO_MUX_INPUT_PULLUP = {
-        0: ( (32, HIGH), (33, HIGH) ),
-        1: ( (45, LOW), (28, HIGH), (29, HIGH) ),
-        2: ( (77, LOW), (34, HIGH), (35, HIGH) ),
-        3: ( (64, LOW), (76, LOW), (16, HIGH), (17, HIGH) ),
-        4: ( (36, HIGH), (37, HIGH) ),
-        5: ( (66, LOW), (18, HIGH), (19, HIGH) ),
-        6: ( (68, LOW), (20, HIGH), (21, HIGH) ),
-        7: ( (38, HIGH), (39, HIGH) ), #TODO update for Fab-G
-        8: ( (40, HIGH), (41, HIGH) ), #TODO update for Fab-G
-        9: ( (70, LOW), (22, HIGH), (23, HIGH) ),
-       10: ( (74, LOW), (26, HIGH), (27, HIGH) ),
-       11: ( (44, LOW), (72, LOW), (24, HIGH), (25, HIGH) ),
-       12: ( (42, HIGH), (43, HIGH) ),
-       13: ( (46, LOW), (30, HIGH), (31, HIGH) ),
-       14: ( (49, HIGH), ), #FIXME
-       15: ( (51, HIGH), ), #FIXME
-       16: ( (53, HIGH), ), #FIXME
-       17: ( (55, HIGH), ), #FIXME
-       18: ( (78, HIGH), (60, HIGH), (57, HIGH) ),
-       19: ( (79, HIGH), (60, HIGH), (59, HIGH) ),
+        0: ((32, HIGH), (33, HIGH)),
+        1: ((45, LOW), (28, HIGH), (29, HIGH)),
+        2: ((77, LOW), (34, HIGH), (35, HIGH)),
+        3: ((64, LOW), (76, LOW), (16, HIGH), (17, HIGH)),
+        4: ((36, HIGH), (37, HIGH)),
+        5: ((66, LOW), (18, HIGH), (19, HIGH)),
+        6: ((68, LOW), (20, HIGH), (21, HIGH)),
+        7: ((38, HIGH), (39, HIGH)),  # TODO update for Fab-G
+        8: ((40, HIGH), (41, HIGH)),  # TODO update for Fab-G
+        9: ((70, LOW), (22, HIGH), (23, HIGH)),
+        10: ((74, LOW), (26, HIGH), (27, HIGH)),
+        11: ((44, LOW), (72, LOW), (24, HIGH), (25, HIGH)),
+        12: ((42, HIGH), (43, HIGH)),
+        13: ((46, LOW), (30, HIGH), (31, HIGH)),
+        14: ((49, HIGH), ),  # FIXME
+        15: ((51, HIGH), ),  # FIXME
+        16: ((53, HIGH), ),  # FIXME
+        17: ((55, HIGH), ),  # FIXME
+        18: ((78, HIGH), (60, HIGH), (57, HIGH)),
+        19: ((79, HIGH), (60, HIGH), (59, HIGH)),
     }
 
     GPIO_MUX_INPUT_PULLDOWN = {
-        0: ( (32, HIGH) ),
-        1: ( (45, LOW), (28, HIGH), (29, LOW) ),
-        2: ( (77, LOW), (34, HIGH), (35, LOW) ),
-        3: ( (64, LOW), (76, LOW), (16, HIGH), (17, LOW) ),
-        4: ( (36, HIGH), (37, LOW) ),
-        5: ( (66, LOW), (18, HIGH), (19, LOW) ),
-        6: ( (68, LOW), (20, HIGH), (21, LOW) ),
-        7: ( (38, HIGH), (39, LOW) ), #TODO update for Fab-G
-        8: ( (40, HIGH), (41, LOW) ), #TODO update for Fab-G
-        9: ( (70, LOW), (22, HIGH), (23, LOW) ),
-       10: ( (74, LOW), (26, HIGH), (27, LOW) ),
-       11: ( (44, LOW), (72, LOW), (24, HIGH), (25, LOW) ),
-       12: ( (42, HIGH), (43, LOW) ),
-       13: ( (46, LOW), (30, HIGH), (31, LOW) ),
-       14: ( (49, LOW), ), #FIXME
-       15: ( (51, LOW), ), #FIXME
-       16: ( (53, LOW), ), #FIXME
-       17: ( (55, LOW), ), #FIXME
-       18: ( (78, HIGH), (60, HIGH), (57, LOW) ),
-       19: ( (79, HIGH), (60, HIGH), (59, LOW) ),
+        0: ((32, HIGH), ),
+        1: ((45, LOW), (28, HIGH), (29, LOW)),
+        2: ((77, LOW), (34, HIGH), (35, LOW)),
+        3: ((64, LOW), (76, LOW), (16, HIGH), (17, LOW)),
+        4: ((36, HIGH), (37, LOW)),
+        5: ((66, LOW), (18, HIGH), (19, LOW)),
+        6: ((68, LOW), (20, HIGH), (21, LOW)),
+        7: ((38, HIGH), (39, LOW)),  # TODO update for Fab-G
+        8: ((40, HIGH), (41, LOW)),  # TODO update for Fab-G
+        9: ((70, LOW), (22, HIGH), (23, LOW)),
+        10: ((74, LOW), (26, HIGH), (27, LOW)),
+        11: ((44, LOW), (72, LOW), (24, HIGH), (25, LOW)),
+        12: ((42, HIGH), (43, LOW)),
+        13: ((46, LOW), (30, HIGH), (31, LOW)),
+        14: ((49, LOW), ),  # FIXME
+        15: ((51, LOW), ),  # FIXME
+        16: ((53, LOW), ),  # FIXME
+        17: ((55, LOW), ),  # FIXME
+        18: ((78, HIGH), (60, HIGH), (57, LOW)),
+        19: ((79, HIGH), (60, HIGH), (59, LOW)),
     }
 
     def __init__(self, debug=False):
         self.debug = debug
-        self.pins_in_use = []
-        self.pins_to_init = []
+        self.pins_in_use = [] #FIXME remove
+        self.pins_to_init = [] #FIXME remove
 
     def digitalWrite(self, pin, state):
+        """Write a value to a GPIO pin.
+
+        The GPIO pin is assumed to be configured as OUTPUT
+
+        Args:
+            pin: Arduino pin number (0-20)
+            state: pin state to be written (LOW-HIGH)
+
+         """
         if pin not in self.GPIO_MAPPING:
             return
         linux_pin = self.GPIO_MAPPING[pin]
         self.__write_value(linux_pin, state)
 
     def digitalRead(self, pin):
+        """Read GPIO pin's state.
+
+        The GPIO pin is assumed to be configured as INPUT
+
+        Args:
+            pin: Arduino pin number (0-20)
+
+        Returns:
+            Current value of the GPIO pin as an Integer
+
+        """
         if pin not in self.GPIO_MAPPING:
             return
         linux_pin = self.GPIO_MAPPING[pin]
         return self.__read_value(linux_pin)
 
     def pinMode(self, pin, mode):
+        """Set mode to GPIO pin`.
+
+        This function must be called before doing any other operation on the
+        pin. It also sets up the muxing needed in Intel® Galileo Gen2 board for
+        the pin to behave as the user wants to.
+
+        Args:
+            pin: Arduino pin number (0-20)
+            mode: pin mode must be:
+                OUTPUT: pin used as output. Use to write into it
+                INPUT: pin used as input (high impedance). Use to read from it
+                INPUT_PULLUP: pin used as input (pullup resistor). Use to read from it
+                INPUT_PULLDOWN: pin used as input (pulldown resistor). Use to read from it
+
+        """
         if pin not in self.GPIO_MAPPING:
             return
 
@@ -224,7 +263,7 @@ class GPIOGalileoGen2(object):
         elif mode in (INPUT, INPUT_PULLUP, INPUT_PULLDOWN):
             self.__set_direction(pin, INPUT)
 
-    def cleanup(self):
+    def cleanup(self):  #FIXME remove
         for pin in self.pins_in_use:
             self.__unexport_pin(pin)
         del self.pins_in_use[:]
--- setup.py
+++ setup.py
@@ -1,47 +1,45 @@
 # -*- coding: utf-8 -*-
+"""
+ Copyright © 2014, Emutex Ltd.
+ All rights reserved.
+ http://www.emutex.com
 
-#########################################################################
-# Copyright © 2014, Emutex Ltd.
-# All rights reserved.
-# http://www.emutex.com
-#
-# Author: Nicolás Pernas Maradei <nicolas.pernas.maradei@emutex.com>
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-#
-# 1. Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-#
-# 2. Redistributions in binary form must reproduce the above copyright
-#    notice, this list of conditions and the following disclaimer in
-#    the documentation and/or other materials provided with the
-#    distribution.
-#
-# 3. All modifications to the source code must be clearly marked as
-#    such. Binary redistributions based on modified source code must
-#    be clearly marked as modified versions in the documentation and/or
-#    other materials provided with the distribution.
-#
-# 4. The name of Emutex Ltd. may not be used to endorse or promote
-#    products derived from this software without specific prior written
-#    permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES INCLUDING,
-# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-#
-#########################################################################
+ Author: Nicolás Pernas Maradei <nicolas.pernas.maradei@emutex.com>
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions
+ are met:
+
+ 1. Redistributions of source code must retain the above copyright
+    notice, this list of conditions and the following disclaimer.
+
+ 2. Redistributions in binary form must reproduce the above copyright
+    notice, this list of conditions and the following disclaimer in
+    the documentation and/or other materials provided with the
+    distribution.
+
+ 3. All modifications to the source code must be clearly marked as
+    such. Binary redistributions based on modified source code must
+    be clearly marked as modified versions in the documentation and/or
+    other materials provided with the distribution.
+
+ 4. The name of Emutex Ltd. may not be used to endorse or promote
+    products derived from this software without specific prior written
+    permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES INCLUDING,
+ BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ POSSIBILITY OF SUCH DAMAGE.
+"""
 
 from distutils.core import setup
 
@@ -50,7 +48,7 @@ setup(
     version='0.1',
     author='Nicolás Pernas Maradei',
     author_email='nicolas.pernas.maradei@emutex.com',
-#     url='',
+    #     url='',
     py_modules=['arduinox86'],
     license='BSD',
     description='A Python module to use basic GPIO on Intel® Arduino capable boards.',
